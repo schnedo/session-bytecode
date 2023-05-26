344fb60ff6b5e535816d3bb492051d7454ddfdcc
schnedo <schnedo@users.noreply.github.com>
2023-05-26 11:55:03 +0200

    start with empty class
    add class files to gitignore
    
    classwithemptyconstructor
    
    code stuff

---
+++ ClassWithConstructor.java
@@ -0,0 +1,8 @@
+public class ClassWithConstructor {
+  public final int foo;
+
+  public ClassWithConstructor() {
+    int someValue = 5;
+    foo = someValue;
+  }
+}
---
+++ ClassWithEmptyConstructor.java
@@ -0,0 +1,4 @@
+public class ClassWithEmptyConstructor {
+
+  public ClassWithEmptyConstructor() {}
+}
